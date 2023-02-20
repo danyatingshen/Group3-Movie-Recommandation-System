f74e82e30c985ef8170c4d2d8d5ce8c3a4bc9104
Vinay Nair <vinwizard@vinays-air.mynetworksettings.com>
2023-02-20 14:32:44 -0500

    Cleaning and Documentation Complete

--- app/Inference/Inference.py
+++ app/Inference/Inference.py
@@ -1,29 +1,24 @@
-# MODEL_PATH = 'model.pkl'
-# DATA_PATH = 'cleaned_data.csv'
-
+'''
+This file is going to be imported in views.py, where it is called whenever a request for recommendations is to be made.
+It takes 'algo', which is the pickle file in which the machine learning algorithm is stored as an object. 
+It also takes a dataframe which has again been read in views.py and passed here as a parameter along with the UID which is requested
+from the server.
+'''
 import pickle as pkl
 import surprise
 import pandas as pd
 
-# df1 = pd.read_csv(DATA_PATH) #loading the movie data to fetch from after inference
-# algo = pkl.load(open(MODEL_PATH, 'rb')) #load these files in the Virtual Machine too
-
-def inference(MODEL_PATH,DATA_PATH,algo,df1,UID):
-  try:
+def inference(algo,df1,UID):
+  try: #Found an existing user.. generate inferences for recommendations
     nn = algo.get_neighbors(algo.trainset.to_inner_uid(UID), k=20)
     print("User Found! Providing Recommendations! \n")
     nn_raw = []
-    # print(df1.columns)
     for x in nn:
       nn_raw.append(algo.trainset.to_raw_uid(x))
-    # print(nn_raw)
     df2 = df1[df1['userID'].isin(nn_raw)]
     op = df2['movieID'].tolist()
-    #print(op)
-    return op #Import karna hai lol
-  except:
+    return op
+  except: #New User has been assigned random movies initially to view
     op = df1['movieID'].sample(n=20).tolist()
     print("Welcome New User! Here's a few movies that you could get started with :) \n")
     return op
-
-  #nn = algo.get_neighbors(algo.trainset.to_inner_uid(UID), k=20)
--- app/views.py
+++ app/views.py
@@ -1,17 +1,21 @@
+#----------------------------------------------------Importing Libraries and Inference Files---------------------------------------------------
 from flask import Flask, render_template, redirect, url_for, request, session, abort, flash
 from .Inference.Inference import inference
 import pandas as pd
 import pickle as pkl
 
 from app import app
-MODEL_PATH='app/Inference/knnmodel.pkl' #app/Inference/model.pkl
+
+#----------------------------------------------------Extracting Resources and Model File---------------------------------------------------
+MODEL_PATH='app/Inference/knnmodel.pkl' 
 FILE_PATH ='app/Inference/cleaned_data.csv'
 
 df1 = pd.read_csv(FILE_PATH) #loading the movie data to fetch from after inference
 algo = pkl.load(open(MODEL_PATH, 'rb')) #load these files in the Virtual Machine too
 
+#----------------------------------------------------Routing for a particular request---------------------------------------------------
 @app.route('/recommend/<userid>')
 def get_recommendation_by_userid(userid:int):
     #return "userId is: {}, movie: x,y,z".format(userid)
-    op = inference(MODEL_PATH,FILE_PATH,algo,df1,int(userid))
+    op = inference(algo = algo,df1 = df1,UID = int(userid))
     return ','.join(op)
