9b2b6e73c1c7600134f97a10276499ca6ad97bc4
Vinay Nair <vinwizard@vinays-air.mynetworksettings.com>
2023-02-20 00:29:19 -0500

    Provides Random Movies for New Users

--- app/Inference/Inference.py
+++ app/Inference/Inference.py
@@ -9,13 +9,21 @@ import pandas as pd
 # algo = pkl.load(open(MODEL_PATH, 'rb')) #load these files in the Virtual Machine too
 
 def inference(MODEL_PATH,DATA_PATH,algo,df1,UID):
-  nn = algo.get_neighbors(algo.trainset.to_inner_uid(UID), k=20)
-  nn_raw = []
-  # print(df1.columns)
-  for x in nn:
-    nn_raw.append(algo.trainset.to_raw_uid(x))
-  # print(nn_raw)
-  df2 = df1[df1['userID'].isin(nn_raw)]
-  op = df2['movieID'].apply(lambda x : x.replace('+',' ')).tolist()
-  #print(op)
-  return op #Import karna hai lol
+  try:
+    nn = algo.get_neighbors(algo.trainset.to_inner_uid(UID), k=20)
+    print("User Found! Providing Recommendations! \n")
+    nn_raw = []
+    # print(df1.columns)
+    for x in nn:
+      nn_raw.append(algo.trainset.to_raw_uid(x))
+    # print(nn_raw)
+    df2 = df1[df1['userID'].isin(nn_raw)]
+    op = df2['movieID'].apply(lambda x : x.replace('+',' ')).tolist()
+    #print(op)
+    return op #Import karna hai lol
+  except:
+    op = df1['movieID'].sample(n=20).apply(lambda x : x.replace('+',' ')).tolist()
+    print("Welcome New User! Here's a few movies that you could get started with :) \n")
+    return op
+
+  #nn = algo.get_neighbors(algo.trainset.to_inner_uid(UID), k=20)
